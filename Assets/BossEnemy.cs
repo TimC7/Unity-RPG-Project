ee886bfd44763a00b5ca710e1c229ae910037b0b
Tim Chester <tjchester7@gmail.com>
2023-07-13 20:34:42 -0700

    boss enemy animations setup

--- Assets/BossEnemy.cs
+++ Assets/BossEnemy.cs
@@ -4,40 +4,6 @@ using UnityEngine;
 
 public class BossEnemy : AttackingEnemy
 {
-    int lastXInt, lastYInt;
+    //int lastXInt, lastYInt;
 
-    protected override void move()
-    {
-                //Debug.Log("We movin");
-        //animator.SetTrigger("Moving");
-        if (Time.time - lastChange > timeBetweenDirectionChanges)
-        {
-            randomDirection();
-            direction.Normalize();
-            lastChange = Time.time;
-        }
-        transform.position += direction * speed * Time.deltaTime;
-    }
-
-    protected override void chase()
-    {
-        //animator.SetTrigger("Moving");
-
-        direction = player.transform.position - transform.position;
-        direction.Normalize();
-
-        lastXDirection = direction.x;
-        lastYDirection = direction.y;
-        animateDirection();
-
-        rb.velocity = direction * speed;
-    }
-
-    protected override void animateDirection()
-    {
-        lastXInt = (int)lastXDirection;
-        lastYInt = (int)lastYDirection;
-        animator.SetInteger("DirectionX", lastXInt);
-        animator.SetInteger("DirectionY", lastYInt);
-    }
 }
